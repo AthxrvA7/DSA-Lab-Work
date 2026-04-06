d20b464443e0a146606acc5859b807661e985d19
Atharva <105702198+AthxrvA7@users.noreply.github.com>
2026-04-06 21:53:35 +0530

    Create LL_middlenode.c

---
+++ LL_middlenode.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int data;
+    struct Node* next;
+};
+
+struct Node* middleNode(struct Node* head){
+    struct Node* slow=head;
+    struct Node* fast=head;
+    while(fast!=NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    return slow;
+}
