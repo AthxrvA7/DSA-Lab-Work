cb340c6c131c6573f1de7f013341bd4b30db69f2
Atharva <105702198+AthxrvA7@users.noreply.github.com>
2026-04-06 21:55:31 +0530

    Update LL_revll.c

--- LL_revll.c
+++ LL_revll.c
@@ -1,3 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int data;
+    struct Node* next;
+};
+
 struct Node* reverse(struct Node* head){
     struct Node* prev=NULL;
     struct Node* curr=head;
